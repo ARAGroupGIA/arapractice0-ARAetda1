92f3b250cc0343cd2e73a003bb87e6f93274a5bf
ARAetda1 <55189190+ARAetda1@users.noreply.github.com>
2019-09-11 14:33:10 +0200

    Version 1 unfinished

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,16 @@
 /* Here, you should entry the required libraries */
-
+#include <stdio.h>
 void main(){
 	/* Here, you should write the source code of your program */
+	int a
+	int b;
+	int sum;
+	
+	printf("enter a number ");
+	scanf("%d",&a);
+	printf("enter a number ");
+	scanf("%d",&b);
+	printf("%d",&a);
+	sum=(a+b);
+	printf("%d",&sum);
 }
