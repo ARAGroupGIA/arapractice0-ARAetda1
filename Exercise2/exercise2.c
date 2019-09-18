d99cc6b8f664c824238093d624167e8476d5f7bf
ARAetda1 <55189190+ARAetda1@users.noreply.github.com>
2019-09-18 12:41:14 +0200

    b
    Version 2 unfinished

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,16 +1,19 @@
 /* Here, you should entry the required libraries */
 #include <stdio.h>
+
 void main(){
 	/* Here, you should write the source code of your program */
-	int a
-	int b;
-	int sum;
+		int a = 3;
+		int b;
+		int sum;
 	
-	printf("enter a number ");
-	scanf("%d",&a);
-	printf("enter a number ");
-	scanf("%d",&b);
-	printf("%d",&a);
-	sum=(a+b);
-	printf("%d",&sum);
+	printf("%d\n",&a);
+		
+	/*	printf("enter a number ");
+		scanf("%d",&a);  Why is a being stored as some really big number?
+		printf("enter a number ");
+		scanf("%d",&b);
+		printf("%d",&a);
+		sum=(a+b);
+		printf("%d",&sum);*/
 }
